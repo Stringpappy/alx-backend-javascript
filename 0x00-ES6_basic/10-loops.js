89a8d25202e929f4b9acc098f75974d85a449f52
stringpappy <jejedunayo70@gmail.com>
2024-10-02 21:07:16 +0000

    ES6_basic

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-  const newArray = [];
+  const myArray = [];
   for (const value of array) {
-    newArray.push(appendString + value);
+    myArray.push(appendString + value);
   }
 
-  return newArray;
+  return myArray;
 }
