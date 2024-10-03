bcc0c83f56e88726191a2ed944d6cf90e6a003c9
stringpappy <jejedunayo70@gmail.com>
2024-10-03 22:52:53 +0000

    ESC6_classes

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,17 +1,30 @@
-expoort default class Currency {
-	constructor(code, name) {
-		this.code = code;
-		this.name = name
-	}
+export default class Currency {
+  constructor(code, name) {
+    this.code = code;
+    this.name = name;
+  }
 
-	/**
-	 *Get the value of code
-	 */
-	get code() {
-		return this._code
-	}
-	/**
-	 * set the value of code
-	 */
+  get code() {
+    return this._code;
+  }
 
+  set code(value) {
+    this._code = value;
+  }
 
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    this._name = value;
+  }
+
+  /**
+   * Creates the full string representation of this Currency.
+   * @returns {String}
+   */
+  displayFullCurrency() {
+    return `${this.name} (${this.code})`;
+  }
+}
