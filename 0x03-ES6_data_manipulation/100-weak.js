1ba664038d32cc2f35b8eb531b78c4af29997442
stringpappy <jejedunayo70@gmail.com>
2024-10-08 23:24:34 +0000

    data manipulation

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,18 +1,4 @@
 export const weakMap = new WeakMap();
-const callPoint = 5;
-
-
-export function queryAPI(endpoint) {
-	if (!weakMap.has(endpoint)) {
-		weakMap.set(endpoint, 0);
-	}
-	weakMap.set(endpoint, weakMap.get(endpoint) + 1);
-	if (weakMap.get(endpoint) >= callPoint) {
-		throw new Error("Endpoint load is high");
-	}
-}
-
-export const weakMap = new WeakMap();
 
 
 const callPoint = 5;
