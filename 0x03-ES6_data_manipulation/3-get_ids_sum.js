53fc7723cfe616949542fab1cb03fb9aff2af234
stringpappy <jejedunayo70@gmail.com>
2024-10-08 22:03:23 +0000

    data manipulation

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,12 +1,8 @@
 export default function getStudentIdsSum(students) {
-	// Check if the argument is an array
-	if (students  instanceof Array) {
-		//store the the Ids in the variable studentIds
-		const studentIds = students.map(student => student.id);
-		// Use reduce to sum the 'id' in the studentIds
-		return studentIds.reduce((acc, num) =>  acc.id  || num.id, 0);
+	if (students instanceof Array) {
+		return students.reduce((acc, num) =>  acc.id  || acc + num.id,
+			0,);
 		
 	};
-	// If the argument is not an array, return
 	return 0;
 };
